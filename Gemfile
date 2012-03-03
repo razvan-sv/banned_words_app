f79564c82354f81881ed4026e16c418d3eb22c7d
Rãzvan Secarã <razvan.secara@softvision.ro>
2012-03-03 11:36:53 +0200

    added rails files

---
+++ Gemfile
@@ -0,0 +1,15 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.1'
+gem 'jquery-rails'
+gem 'thin'
+
+gem "mongoid", "~> 2.4"
+gem "bson_ext", "~> 1.5"
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'therubyracer'
+  gem 'uglifier', '>= 1.0.3'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BannedWordsApp::Application.config.secret_token = 'c13a1e14fd4f7053adc6917b7399029f8280dedae7828037daab2ef86f27689e283855801517083ccb1e0633151e81a4412d992fd2424b192ad0729b77860dd3'
